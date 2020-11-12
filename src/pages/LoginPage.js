2f3a850394558262e1767eedc536b6418750ddea
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-11-12 03:03:53 -0300

    Email input done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import { LinearGradient } from 'expo-linear-gradient'
 import firebase from 'firebase'
 import { render } from 'react-dom'
 
-import { View, StyleSheet, Text, Image, TextInput } from 'react-native'
+import { View, StyleSheet, Text, Image, TextInput, Platform } from 'react-native'
 
 class LoginPage extends React.Component {
     render() {
@@ -13,6 +13,11 @@ class LoginPage extends React.Component {
                 <LinearGradient colors={['#011228', '#0E487E']} start={{x: 0.8, y: 0.1}} end={{x: 0.4, y: 1}} style={styles.gradient}>
                     <Image source={require('../../images/steamIcon.png')} style={styles.logo}/>
                     <Text style={styles.slogan}>Let the games begin</Text>
+
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.emailText}>Email</Text>
+                        <TextInput style={styles.textInput} />
+                    </View>
                 </LinearGradient>
             </View>
         )
@@ -38,6 +43,20 @@ const styles = StyleSheet.create({
         marginTop: 35,
         fontSize: 13
     },
+    inputContainer: {
+        marginTop: 60
+    },
+    textInput: {
+        width: 230,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#71A6D6',
+    },
+    emailText: {
+        color: 'white',
+        marginLeft: 6,
+        fontSize: 11
+    }
 })
 
 export default LoginPage

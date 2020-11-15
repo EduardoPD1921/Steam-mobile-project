2792aa8519dfa98cef6e4744d7697f1e719a8df6
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-11-14 21:56:33 -0300

    Redux first setup done

--- src/SteamApp.js
+++ src/SteamApp.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import Router from './Router'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers'
+
+import devToolsEnhancer from 'remote-redux-devtools'
+
+const store = createStore(rootReducer, devToolsEnhancer())
+
 export default props => {
     return (
-        <Router />
+        <Provider store={store}>
+            <Router />
+        </Provider>
     )
 }

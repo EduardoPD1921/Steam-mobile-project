4f9abcac5b45480618a0eb32f73beff8211b2838
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-11-19 12:51:48 -0300

    SignupPage stylization of inputs and checkbox done

---
+++ src/components/SignupInput.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { TextInput, Text, View, StyleSheet } from 'react-native'
+
+export default ({ text, secureTextEntry }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.inputType}>
+                {text}
+            </Text>
+            <TextInput secureTextEntry={secureTextEntry} style={styles.textInput}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignContent: 'flex-start'
+    },
+    inputType: {
+        color: 'white',
+        fontSize: 11
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderColor: '#DCDEF4',
+        width: 238,
+        color: 'white'
+    }
+})

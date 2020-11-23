37fb8df17d8c1215f16f5c68dfd889ddcb2df167
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-11-23 13:14:21 -0300

    DrawerNavigation implemented and changed the way of how stackNavigator works

--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,61 @@
 import React from 'react'
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import StorePage from './pages/StorePage'
+import FriendsPage from './pages/FriendsPage'
+import { Header } from 'react-native/Libraries/NewAppScreen'
 
-const AppNavigator = createStackNavigator({
-    'Login': {
-        screen: LoginPage,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    'Signup': {
-        screen: SignupPage,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    'Store': {
-        screen: StorePage
-    }
-})
+// const AppNavigator = createStackNavigator({
+//     'Login': {
+//         screen: LoginPage,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     'Signup': {
+//         screen: SignupPage,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     'Store': {
+//         screen: StorePage,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     }
+// })
 
-const AppContainer = createAppContainer(AppNavigator)
+// const AppContainer = createAppContainer(AppNavigator)
 
-export default AppContainer
+// export default AppContainer
+
+const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function DrawerRoutes() {
+    return (
+        <Drawer.Navigator initialRouteName='Store'>
+            <Drawer.Screen name='Store' component={StorePage} />
+            <Drawer.Screen name='Friends' component={FriendsPage} />
+        </Drawer.Navigator>
+    )
+}
+
+function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name='Login' component={LoginPage} />
+                <Stack.Screen name='Signup' component={SignupPage} />
+                <Stack.Screen name='Store' component={DrawerRoutes} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigator

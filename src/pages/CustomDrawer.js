38cfd38ffa2f04adcc33e5d355f349a0f4eefa4b
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-11-30 22:01:54 -0300

    userStatus dynamic done

--- src/pages/CustomDrawer.js
+++ src/pages/CustomDrawer.js
@@ -2,61 +2,110 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { DrawerItem } from '@react-navigation/drawer'
 import { Avatar } from 'react-native-paper'
+import { Picker } from '@react-native-picker/picker'
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { connect } from 'react-redux'
 
 import { userLogout } from '../actions'
 
-function logOut(props) {
-    props.userLogout()
-    props.navigation.navigate('Login')
-}
+class CustomDrawer extends React.Component {
+    constructor(props) {
+        super(props)
 
-function CustomDrawer(props) {
-    return (
-        <View style={styles.mainContainer}>
-            <View style={styles.firstHalf}>
-                <View style={styles.userInformation}>
-                    <Avatar.Image source={require('../../images/profilePicture.png')} size={50} />
-                    <Text style={styles.userId}>{props.userName}</Text>
+        this.state = {
+            userStatus: 'online'
+        }
+    }
+
+    logOut() {
+        this.props.userLogout()
+        this.props.navigation.navigate('Login')
+    }
+
+    getUserStatusColor() {
+        switch (this.state.userStatus) {
+            case 'online':
+                return 'green'
+            case 'idle':
+                return 'orange'
+            case 'offline':
+                return 'gray'
+            default:
+                return 'green'
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <View style={styles.firstHalf}>
+                    <View style={styles.userInformation}>
+                        <Avatar.Image source={require('../../images/profilePicture.png')} size={50} />
+                        <Text style={styles.userId}>{this.props.userName}</Text>
+                        <Icon 
+                            style={styles.userStatus} 
+                            color={this.getUserStatusColor()} 
+                            name='checkbox-blank-circle' 
+                            size={10} 
+                        />
+                        <Picker 
+                            selectedValue={this.state.userStatus} 
+                            onValueChange={(itemValue, itemIndex) => this.setState({ userStatus: itemValue })} 
+                            style={styles.dropDownStatus}
+                        >
+                            <Picker.Item label='Online' value='online' />
+                            <Picker.Item label='Idle' value='idle' />
+                            <Picker.Item label='Offline' value='offline' />
+                        </Picker>
+                    </View>
+
+                    <DrawerItem
+                        onPress={() => this.props.navigation.navigate('Store')} 
+                        label='Store' 
+                        labelStyle={{ color: 'white' }} 
+                        icon={() => <Icon 
+                        color='white' 
+                        name='cart-outline' 
+                        size={20} />} 
+                    />
+
+                    <DrawerItem
+                        onPress={() => this.props.navigation.navigate('Friends')} 
+                        label='Friends' 
+                        labelStyle={{ color: 'white' }} 
+                        icon={() => <Icon 
+                        color='white' 
+                        name='face-profile' 
+                        size={20} />} 
+                    />
+
+                    <DrawerItem
+                        onPress={() => this.props.navigation.navigate('Wishlist')} 
+                        label='Wishlist' 
+                        labelStyle={{ color: 'white' }} 
+                        icon={() => <Icon 
+                        color='white' 
+                        name='format-list-bulleted' 
+                        size={20} />} 
+                    />
                 </View>
 
-                <DrawerItem
-                    onPress={() => props.navigation.navigate('Store')} 
-                    label='Store' 
-                    labelStyle={{ color: 'white' }} 
-                    icon={() => <Icon 
-                    color='white' 
-                    name='cart-outline' 
-                    size={20} />} 
-                />
-
-                <DrawerItem
-                    onPress={() => props.navigation.navigate('Friends')} 
-                    label='Friends' 
-                    labelStyle={{ color: 'white' }} 
-                    icon={() => <Icon 
-                    color='white' 
-                    name='face-profile' 
-                    size={20} />} 
-                />
+                <View style={styles.lastHalf}>
+                    <DrawerItem
+                        onPress={() => this.logOut()}
+                        style={styles.logOut} 
+                        labelStyle={{ color: 'white' }} 
+                        label='Log out' 
+                        icon={() => <Icon 
+                        color='white' 
+                        name='exit-to-app' 
+                        size={16} />} 
+                    />
+                </View>
             </View>
-
-            <View style={styles.lastHalf}>
-                <DrawerItem
-                    onPress={() => logOut(props)}
-                    style={styles.logOut} 
-                    labelStyle={{ color: 'white' }} 
-                    label='Log out' 
-                    icon={() => <Icon 
-                    color='white' 
-                    name='exit-to-app' 
-                    size={16} />} 
-                />
-            </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -84,10 +133,19 @@ const styles = StyleSheet.create({
         paddingLeft: 10,
         fontSize: 20
     },
+    userStatus: {
+        alignSelf: 'center',
+        marginLeft: 10
+    },
     logOut: {
         borderTopWidth: 0.1,
         borderTopColor: 'gray',
         alignSelf: 'stretch' 
+    },
+    dropDownStatus: {
+        alignSelf: 'center',
+        width: 40,
+        height: 40
     }
 })
 
